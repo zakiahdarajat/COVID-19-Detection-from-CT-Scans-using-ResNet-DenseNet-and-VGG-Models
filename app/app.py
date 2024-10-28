3c75233d9cf56bd9171e31d7abedb1e9ef73c468
Zakiah D. Nurfajrin <139196241+zakiahdarajat@users.noreply.github.com>
2024-10-29 04:26:11 +0900

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import numpy as np
+import os
+from model import image_pre,predict
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+#import pandas as pd
+
+app = Flask(__name__)
+
+
+UPLOAD_FOLDER = '/Users/adityavs14/Documents/Internship/Pianalytix/COVID/app/static'
+ALLOWED_EXTENSIONS = set(['png'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def home():
+    return render_template('index.html') 
+
+@app.route('/', methods=['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file1' not in request.files:
+            return 'there is no file1 in form!'
+        file1 = request.files['file1']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], 'input.png')
+        file1.save(path)
+        data = image_pre(path)
+        s = predict(data)
+        if s == 1:
+            result = 'No COVID detected'
+        else:
+            result = 'COVID detected'
+    return render_template('index.html',result=result) 
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app/model.py
@@ -0,0 +1,23 @@
+import keras
+import numpy as np
+from matplotlib.pyplot import imshow
+from PIL import Image, ImageOps
+
+
+base = '/Users/adityavs14/Documents/Internship/Pianalytix/COVID/app'
+model = keras.models.load_model(f'{base}/CovidTest.h5')
+
+def image_pre(path):
+    print(path)
+    data = np.ndarray(shape=(1,128, 128, 1), dtype=np.float32)
+    size = (128,128)
+    image = Image.open(path)
+    image = ImageOps.grayscale(image)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    image_array = np.asarray(image)
+    data = image_array.reshape((-1,128,128,1))/255
+    return data
+
+def predict(data):
+    prediction = model.predict(data)
+    return np.round(prediction[0][0])
